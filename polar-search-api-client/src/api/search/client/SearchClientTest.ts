4dc4733684178e23645fd225493e81b843246773
burton <burton@getpolarized.io>
2019-10-12 13:24:00 -0700

    I have a basic search API and REST client I can use with it so that the desktop client can call the REST API...

--- polar-search-api-client/src/api/search/client/SearchClientTest.ts
+++ polar-search-api-client/src/api/search/client/SearchClientTest.ts
@@ -1,11 +1,13 @@
 import {SearchClient} from "./SearchClient";
 
 
-describe('SearchClient', function() {
+xdescribe('SearchClient', function() {
 
     it("basic", async function() {
         this.timeout(50000);
         const results = await SearchClient.exec({q: 'bitcoin', target: 'arxiv'});
+
+        console.log(JSON.stringify(results, null, "  "));
 
     });
 
