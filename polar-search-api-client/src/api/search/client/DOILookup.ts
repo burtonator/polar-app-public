6fdce398ba4c67880a9a7b375f611ad57946fbc9
burtonator <burtonator@gmail.com>
2019-10-16 10:32:55 -0700

    ...

--- polar-search-api-client/src/api/search/client/DOILookup.ts
+++ polar-search-api-client/src/api/search/client/DOILookup.ts
@@ -2,12 +2,15 @@ import {search} from "polar-search-api/src/api/search/Search";
 import DOIStr = search.DOIStr;
 import {SearchClient} from "./SearchClient";
 
+/**
+ * Lookup the metadata and download URL for a specific DOU
+ */
 export class DOILookup {
 
     public static async lookup(doi: DOIStr): Promise<search.Results> {
 
-        // TODO: fatcat supports DOI too...
-        return await SearchClient.exec({q: doi, target: 'unpaywall'})
+        // TODO: fatcat supports DOI too just not always the download URL.
+        return await SearchClient.exec({q: doi, target: 'unpaywall'});
 
     }
 
