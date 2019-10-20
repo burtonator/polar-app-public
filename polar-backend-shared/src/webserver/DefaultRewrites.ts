8ad44b9f48860dcfea03d1c287c91243feb76dd7
burton <burton@getpolarized.io>
2019-10-20 09:34:19 -0700

    moving to polar-app-public.

---
+++ polar-backend-shared/src/webserver/DefaultRewrites.ts
@@ -0,0 +1,60 @@
+import {Rewrite} from "../polar-shared-webserver/src/webserver/Rewrites";
+
+export class DefaultRewrites {
+
+    public static create(): ReadonlyArray<Rewrite> {
+
+        // TODO: for now the source is a regular expression.  Make it into
+        // the same slug format that ReactRouter uses in the future.
+
+        return [
+            {
+                "source": "/",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/index.html",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/group/:group/highlights",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/group/:group/docs",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/group/:group/highlight/:id",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/group/:group",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/groups",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/groups/create",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/user/:user",
+                "destination": "/apps/repository/index.html"
+            },
+            {
+                "source": "/login.html",
+                "destination": "/apps/repository/login.html"
+            },
+
+            {
+                "source": "/pdfjsWorker-bundle.js",
+                "destination": "/web/dist/pdfjsWorker-bundle.js"
+            }
+        ];
+
+    }
+
+}
