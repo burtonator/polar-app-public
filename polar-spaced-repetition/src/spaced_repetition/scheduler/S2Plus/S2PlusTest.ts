e6214dcb08b795e2629b5d164eaf13df6f123e79
burton <burton@getpolarized.io>
2019-10-26 08:29:41 -0700

    docs..

--- polar-spaced-repetition/src/spaced_repetition/scheduler/S2Plus/S2PlusTest.ts
+++ polar-spaced-repetition/src/spaced_repetition/scheduler/S2Plus/S2PlusTest.ts
@@ -5,12 +5,12 @@ import {oneDayAgo} from './DateConstants';
 import {today} from './DateConstants';
 import {Dates} from './Dates';
 import {S2Plus} from './S2Plus';
-import {TestingTime} from '../../../../polar-shared/src/test/TestingTime';
 import {DEFAULT_DIFFICULTY} from './S2Plus';
 import {DEFAULT_INTERVAL} from './S2Plus';
 import {Days} from './Dates';
 import {Difficulty} from './S2Plus';
 import {twoDaysAgo} from './DateConstants';
+import {TestingTime} from "polar-shared/src/test/TestingTime";
 
 const testData = [
     {
