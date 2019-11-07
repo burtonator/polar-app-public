30dd9185c7b77d467ef12683ac2734f1e7499959
burton <burton@getpolarized.io>
2019-11-06 18:37:52 -0800

    ... parse closes...

--- polar-spaced-repetition/src/spaced_repetition/scheduler/util/ClozeParser.ts
+++ polar-spaced-repetition/src/spaced_repetition/scheduler/util/ClozeParser.ts
@@ -1,6 +1,8 @@
+import {Tuples} from "../../../../../../polar-bookshelf/web/js/util/Tuples";
+
 export class ClozeParser {
 
-    public static parse(input: string): ReadonlyArray<ClozeRegion> {
+    public static toClozeRegions(input: string): ReadonlyArray<ClozeRegion> {
 
         const regex = /{{c([0-9]+)::(.*?)}}/g;
         const matches = this.matches(input, regex);
@@ -13,8 +15,9 @@ export class ClozeParser {
             const length = match[0].length;
 
             const cloze: ClozeRegion = {
-                id, occluded, offset, length,
-                type: 'cloze'
+                id, text: occluded, offset, length,
+                type: 'cloze',
+                subtype: 'none'
             };
 
             return cloze;
@@ -22,6 +25,94 @@ export class ClozeParser {
         };
 
         return matches.map(current => toCloze(current));
+
+    }
+
+    /**
+     * Parse this out into 'regions' that can just be raw text too.
+     */
+    public static toRegions(input: string): ReadonlyArray<Region> {
+
+        const clozeRegions = this.toClozeRegions(input);
+
+        const computeFirstTextRegion = (curr: ClozeRegion) => {
+
+            const offset = 0;
+            const length = curr.offset;
+            const text = input.substring(offset, offset + length);
+
+            const textRegion: TextRegion = {
+                type: 'text',
+                subtype: 'first',
+                text, offset, length
+            };
+
+            return textRegion;
+        };
+
+        const computeLastTextRegion = (curr: ClozeRegion) => {
+            const offset = curr.offset + curr.length;
+            const length = input.length - offset;
+            const text = input.substring(offset, offset + length);
+            const textRegion: TextRegion = {
+                type: 'text',
+                subtype: 'last',
+                text, offset, length
+            };
+            return textRegion;
+        };
+
+
+        const computeMiddleTextRegion = (curr: ClozeRegion, next: ClozeRegion) => {
+            const offset = curr.offset + curr.length;
+            const length = next.offset - offset;
+            const text = input.substring(offset, offset + length);
+            const textRegion: TextRegion = {
+                type: 'text',
+                subtype: 'mid',
+                text, offset, length
+            };
+            return textRegion;
+
+        };
+
+
+        if (clozeRegions.length === 0) {
+
+            // TODO: if there is no first or last?
+
+            const result: TextRegion = {
+                type: 'text',
+                subtype: 'full',
+                text: input,
+                offset: 0,
+                length: input.length
+            };
+
+            return [result];
+        }
+
+        const result: Region[] = [];
+
+        const tuples = Tuples.createSiblings(clozeRegions);
+
+        for (const tuple of tuples) {
+
+            if (! tuple.prev) {
+                result.push(computeFirstTextRegion(tuple.curr));
+            }
+
+            result.push(tuple.curr);
+
+            if (tuple.next) {
+                result.push(computeMiddleTextRegion(tuple.curr, tuple.next));
+            } else {
+                result.push(computeLastTextRegion(tuple.curr));
+            }
+
+        }
+
+        return result;
 
     }
 
@@ -47,9 +138,25 @@ export class ClozeParser {
 export type RegionType = 'text' | 'cloze';
 
 export interface Region {
+
+    /**
+     * Offset in the parent.
+     */
     readonly offset: number;
+
+    /**
+     * Length of the text in the parent.
+     */
     readonly length: number;
+
     readonly type: RegionType;
+
+    readonly subtype: string;
+
+    /**
+     * The text representation of this region as a substring of the parent.
+     */
+    readonly text: string;
 }
 
 /**
@@ -71,6 +178,6 @@ export interface ClozeRegion extends Region{
     /**
      * The occluded text that should be initially hidden.
      */
-    readonly occluded: string;
+    readonly text: string;
 
 }
--- polar-spaced-repetition/src/spaced_repetition/scheduler/util/ClozeParserTest.ts
+++ polar-spaced-repetition/src/spaced_repetition/scheduler/util/ClozeParserTest.ts
@@ -3,9 +3,9 @@ import {ClozeParser} from "./ClozeParser";
 
 describe('ClozeParser', function() {
 
-    it("basic", function () {
+    it("toClozeRegions", function () {
 
-        assertJSON(ClozeParser.parse('Is Your {{c1::Startup}} Idea Taken?'), [
+        assertJSON(ClozeParser.toClozeRegions('Is Your {{c1::Startup}} Idea Taken?'), [
             {
                 "id": 1,
                 "occluded": "Startup",
@@ -15,7 +15,7 @@ describe('ClozeParser', function() {
             }
         ]);
 
-        assertJSON(ClozeParser.parse('Is Your {{c1::Startup}} Idea Taken?  Not by {{c2::Microsoft}}.'), [
+        assertJSON(ClozeParser.toClozeRegions('Is Your {{c1::Startup}} Idea Taken?  Not by {{c2::Microsoft}}.'), [
             {
                 "id": 1,
                 "occluded": "Startup",
@@ -33,6 +33,77 @@ describe('ClozeParser', function() {
         ]);
 
     });
+
+
+    it("toRegions", function () {
+
+        assertJSON(ClozeParser.toRegions('Is Your {{c1::Startup}} Idea Taken?'), [
+            {
+                "type": "text",
+                "subtype": "first",
+                "text": "Is Your ",
+                "offset": 0,
+                "length": 8
+            },
+            {
+                "id": 1,
+                "text": "Startup",
+                "offset": 8,
+                "length": 15,
+                "type": "cloze",
+                "subtype": "none"
+            },
+            {
+                "type": "text",
+                "subtype": "last",
+                "text": " Idea Taken?",
+                "offset": 23,
+                "length": 12
+            }
+        ]);
+
+        assertJSON(ClozeParser.toRegions('Is Your {{c1::Startup}} Idea Taken?  Not by {{c2::Microsoft}}.'), [
+            {
+                "type": "text",
+                "subtype": "first",
+                "text": "Is Your ",
+                "offset": 0,
+                "length": 8
+            },
+            {
+                "id": 1,
+                "text": "Startup",
+                "offset": 8,
+                "length": 15,
+                "type": "cloze",
+                "subtype": "none"
+            },
+            {
+                "type": "text",
+                "subtype": "mid",
+                "text": " Idea Taken?  Not by ",
+                "offset": 23,
+                "length": 21
+            },
+            {
+                "id": 2,
+                "text": "Microsoft",
+                "offset": 44,
+                "length": 17,
+                "type": "cloze",
+                "subtype": "none"
+            },
+            {
+                "type": "text",
+                "subtype": "last",
+                "text": ".",
+                "offset": 61,
+                "length": 1
+            }
+        ]);
+
+    });
+
 
 });
 
