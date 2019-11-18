0234e30638945feb1266dd00f5f23a381d3ab519
burton <burton@getpolarized.io>
2019-11-17 17:29:28 -0800

    Fixed font size which looked bad on Firefox...

--- polar-firebase/src/firebase/om/UserMachines.ts
+++ polar-firebase/src/firebase/om/UserMachines.ts
@@ -1,7 +1,16 @@
+import {PlatformStr} from "polar-shared/src/util/Platforms";
+import {MachineID} from "../../../../../polar-bookshelf/web/js/util/MachineIDs";
+import {UserIDStr} from "../Collections";
+
 export class UserMachines {
 
 }
 
+export interface UserMachineInit {
+    readonly id: MachineID;
+    readonly platform: PlatformStr;
+}
+
 export interface UserMachine {
-    // readonly platform:
+    readonly user_id: UserIDStr;
 }
