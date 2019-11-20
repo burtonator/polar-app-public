a01a9c5caf23be247549c757522d75a1c4457b25
burton <burton@getpolarized.io>
2019-11-20 09:53:11 -0800

    make sure to return release data in descending form.
    more release metadata...

--- polar-release-metadata/src/ReleaseMetadatas.ts
+++ polar-release-metadata/src/ReleaseMetadatas.ts
@@ -1,13 +1,26 @@
 import {VersionStr} from "polar-shared/src/util/Version";
 import {HTMLStr} from "polar-shared/src/util/Strings";
+import * as semver from 'semver';
 
 // just load the version on disk that's been pre-compiled.
 const metadata = require("./release-metadata.json");
 
 export class ReleaseMetadatas {
 
+    /**
+     * Get the release metadata, sorted by release version, descending.
+     */
     public static get(): ReadonlyArray<ReleaseMetadata> {
-        return metadata;
+        return this.sorted(metadata);
+    }
+
+    /**
+     * Sort the release data by release version, descending.
+     */
+    public static sorted(releases: ReadonlyArray<ReleaseMetadata>): ReadonlyArray<ReleaseMetadata> {
+        return [...releases]
+            .sort((a, b) => semver.compare(a.release, b.release))
+            .reverse();
     }
 
 }
