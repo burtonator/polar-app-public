e11069f3c6f82c0fce3efbfdc55ef48763fa55f0
burtonator <burtonator@gmail.com>
2019-11-28 08:46:30 -0800

    Rename to FileSavers so we can separate it from the main interface.
    The buttons and exporter work now.
    
    Annotation export now works from the main UI...

--- polar-file-saver/src/FileSavers.ts
+++ polar-file-saver/src/FileSavers.ts
@@ -5,12 +5,10 @@ import { saveAs } from 'file-saver';
  *
  * https://github.com/eligrey/FileSaver.js
  */
-export class FileSaver {
+export class FileSavers {
 
     /**
      * Trigger a file download using the specified filename and blob.
-     * @param blob
-     * @param filename
      */
     public static saveAs(blob: Blob, filename: string) {
         saveAs(blob, filename);
