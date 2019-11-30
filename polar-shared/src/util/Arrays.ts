c4e7579d464f02df4d665c96567d2ad0cf2d86c5
burtonator <burtonator@gmail.com>
2019-11-29 19:16:25 -0800

    reworking it so we can determine the selected node

--- polar-shared/src/util/Arrays.ts
+++ polar-shared/src/util/Arrays.ts
@@ -264,7 +264,7 @@ export class Arrays {
 
         if (a === b) return true;
         if (a == null || b == null) return false;
-        if (a.length != b.length) return false;
+        if (a.length !== b.length) return false;
 
         // If you don't care about the order of the elements inside
         // the array, you should sort both arrays here.
