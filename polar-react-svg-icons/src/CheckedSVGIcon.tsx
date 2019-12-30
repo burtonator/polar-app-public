9822ca29340d46065beed1c45042a8b46a3a458b
burtonator <burtonator@gmail.com>
2019-12-30 11:46:52 -0800

    removed XML header to this SVG.
    chrome XVG icon.
    
    cleanup to remove pages that were bloated and not used
    
    removed embedded PNG images now that I figured out how to do SVG.

--- polar-react-svg-icons/src/CheckedSVGIcon.tsx
+++ polar-react-svg-icons/src/CheckedSVGIcon.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 
-const SVG = `<?xml version="1.0" encoding="iso-8859-1"?>
-<!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+const SVG = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 \t viewBox="0 0 504.12 504.12" style="enable-background:new 0 0 504.12 504.12;" xml:space="preserve">
 <circle style="fill:#3DB39E;" cx="252.06" cy="252.06" r="252.06"/>
 <path style="fill:#37A18E;" d="M463.163,114.609L240.246,345.403l0.394,24.812h10.24l241.428-194.56
