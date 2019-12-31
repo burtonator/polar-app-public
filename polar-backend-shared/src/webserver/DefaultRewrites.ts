714253c0962ef5b93260e8b875c7ca05c4f9f64a
burtonator <burtonator@gmail.com>
2019-12-31 13:46:33 -0800

    Fixed navigation links I think...

--- polar-backend-shared/src/webserver/DefaultRewrites.ts
+++ polar-backend-shared/src/webserver/DefaultRewrites.ts
@@ -20,6 +20,7 @@ export class DefaultRewrites {
                     "/whats-new",
                     "/logout",
                     "/login",
+                    "/logs",
                     "/configured",
                     "/invite",
                     "/premium",
