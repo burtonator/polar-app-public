d9440b02dafa553daca0f31b5a2b7550acc87f0e
burtonator <burtonator@gmail.com>
2020-01-22 14:12:51 -0800

    refactoring a bit to make this more testable...

--- polar-pagemarks-auto/src/AutoPagemarkerTest.ts
+++ polar-pagemarks-auto/src/AutoPagemarkerTest.ts
@@ -4,6 +4,38 @@ import {PageVisibility, Viewport, ViewVisibility} from "./AutoPagemarkCalculator
 import {TestingTime} from "polar-shared/src/test/TestingTime";
 import {assertJSON} from "polar-test/src/test/Assertions";
 
+const viewport: Viewport = {
+    top: 0,
+    bottom: 1100
+};
+
+const createPageVisibility = (page: PageID,
+                              top: number,
+                              bottom: number,
+                              perc: number): PageVisibility => {
+
+    return {
+        id: page,
+        top,
+        bottom,
+        perc
+    };
+
+};
+
+const createViewVisibility = (): ViewVisibility => {
+    const computed = Date.now();
+
+    return {
+        viewport,
+        visibilities: [
+            createPageVisibility(1, 0, 1100, 100),
+            createPageVisibility(2, 1100, 2200, 0),
+        ],
+        computed
+    };
+};
+
 describe('AutoPagemarker', function() {
 
     beforeEach(() => {
@@ -18,37 +50,6 @@ describe('AutoPagemarker', function() {
 
         const pagemarker = new AutoPagemarker(NULL_FUNCTION);
 
-        const viewport: Viewport = {
-            top: 0,
-            bottom: 1100
-        };
-
-        const computed = Date.now();
-
-        const createPageVisibility = (page: PageID,
-                                      top: number,
-                                      bottom: number,
-                                      perc: number): PageVisibility => {
-
-            return {
-                id: page,
-                top,
-                bottom,
-                perc
-            };
-
-        };
-
-        const createViewVisibility = (): ViewVisibility => {
-            return {
-                viewport,
-                visibilities: [
-                    createPageVisibility(1, 0, 1100, 100),
-                    createPageVisibility(2, 1100, 2200, 0),
-                ],
-                computed
-            };
-        };
 
         const viewVisibility = createViewVisibility();
 
