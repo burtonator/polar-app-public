8adf8c60b44a164916b297592c692ee3d7d5dbdb
burtonator <burtonator@gmail.com>
2020-01-28 08:38:30 -0700

    more work on auto pagemarker so that we can work with the html mode
    improved invitation system but not ready to turn on yet.

--- polar-pagemarks-auto/src/AutoPagemarker.ts
+++ polar-pagemarks-auto/src/AutoPagemarker.ts
@@ -89,11 +89,11 @@ export class AutoPagemarker {
                 private mode: AutoPagemarkerMode = 'full') {
     }
 
-    public compute(curr: ViewVisibility): ComputeResult {
+    public compute(viewVisibility: ViewVisibility): ComputeResult {
 
         const now = Date.now();
 
-        const visible = ViewVisibilityCalculator.visible(curr.visibilities);
+        const visible = ViewVisibilityCalculator.visible(viewVisibility.visibilities);
 
         const createResult = (strategy: ComputeStrategy,
                               pagemarked: number | undefined): ComputeResult => {
@@ -164,12 +164,15 @@ export class AutoPagemarker {
 
         interface Coverage {
             readonly perc: Percentage100;
+            readonly state: 'created' | 'updated' | 'jumped';
         }
+
+        // there are really only three states 'none' | 'paged' or 'jumped'
 
         /**
          * Compute what percentage of the page is covered and whether we should emit.
          */
-        const computeCoverage = (): Coverage | undefined => {
+        const computeCoverage = (): Coverage => {
 
             switch (this.mode) {
 
@@ -179,9 +182,22 @@ export class AutoPagemarker {
                     // view.
 
                     if ((currPageID - 1) === prevPageID) {
-                        return {perc: 100};
+                        return {
+                            state: 'created',
+                            perc: 100
+                        };
+                    } else if (currPageID === prevPageID) {
+
+                        return {
+                            state: 'updated',
+                            perc: 100
+                        };
+
                     } else {
-                        return undefined;
+                        return {
+                            state: 'jumped',
+                            perc: 0
+                        };
                     }
 
                 case "partial":
@@ -190,7 +206,32 @@ export class AutoPagemarker {
                     // and since it only has one page we just adjust the coverage
                     // to be JUST up to the beginning of the viewport.
 
+                    // then map these back into the original percentage of page
+                    // model.
+
+                    // TODO how do I compare this to the viewport size? if the page
+                    // is zoomed in/out?
+
+                    // TODO a simpler way to do this could be to take the current
+                    // viewport height, take 20 of this, break the 'page' up into
+                    // chunks (like pages) and then map them back to the original
+
+                    const height = viewVisibility.viewport.bottom - viewVisibility.viewport.top;
+
+                    // FIXME: use  pageVisibility.perc to determine which 'page' is marked...
+                    //
+                    // map BACK ot the original??
+
+                    // FIXME: maybe I can do this in the view computation code, compute a
+                    // CUSTOM view and page size, then allow this engine to work, then I listen
+                    // to the 'pages' that are marked and map them BACK to the real pages.
+
+                    // map these to virtual pages from the height of ALL the main page
+                    // and compare that to the viewport
+
+
                     return {
+                        state: 'updated',
                         perc: pageVisibility.perc * 100
                     };
 
@@ -200,25 +241,35 @@ export class AutoPagemarker {
 
         const coverage = computeCoverage();
 
-        if (coverage) {
-            // we have advanced one page exactly and the previous page
-            // is now moved forward.
+        // FIXME: jumped is currently defined as one page BUT the html view is only 1 page so therefore
+        // there will never be any 'jumps.  We should consider a jump > 1x of a viewport.
+        //
+        // FIXME: another issue with HTML mode is that a 'continuation' where we're starting a new pagemark
+        // deep down the page needs to be starting at a percentage.
 
-            this.callback({
-                origin: this.position.origin,
-                page: prevPageID,
-                perc: coverage.perc
-            });
+        switch (coverage.state) {
 
-            return updatePosition('created', prevPageID);
+            case "created":
+                // we have advanced one page exactly and the previous page
+                // is now moved forward.
 
-        } else if (currPageID === prevPageID) {
-            return updatePosition('updated');
-        } else {
-            log.debug("jumped due to pages: ", {currPageID, prevPageID});
+                this.callback({
+                    origin: this.position.origin,
+                    page: prevPageID,
+                    perc: coverage.perc
+                });
 
-            this.position = undefined;
-            return updatePosition('jumped');
+                return updatePosition('created', prevPageID);
+
+            case "updated":
+                return updatePosition('updated');
+
+            case "jumped":
+                log.debug("jumped due to pages: ", {currPageID, prevPageID});
+
+                this.position = undefined;
+                return updatePosition('jumped');
+
         }
 
     }
