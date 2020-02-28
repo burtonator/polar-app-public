954ea57d6c5276dcb2a7f26467689fa36f9c69f5
burtonator <burtonator@gmail.com>
2020-02-28 14:17:27 -0700

    bad import

--- polar-text-highlighter/src/TextHighlighter.ts
+++ polar-text-highlighter/src/TextHighlighter.ts
@@ -1,3 +1,49 @@
+import {IRect, MutableIRect} from "polar-shared/src/util/rects/IRect";
+
+function getScrollParent(element: HTMLElement | undefined): HTMLElement | undefined {
+
+    if (! element) {
+        return undefined;
+    }
+
+    if (element.scrollHeight > element.clientHeight) {
+        return element;
+    } else {
+        return getScrollParent(element.parentElement || undefined);
+    }
+
+}
+
+export interface ITextHighlighterOpts {
+    readonly id: string;
+}
+
 export class TextHighlighter {
+
+    private static applyHighlight(opts: ) {
+
+    }
+
+}
+
+class IntersectRects {
+
+    public static compute(a: IRect, b: IRect): IRect {
+
+        const result: MutableIRect = {
+            top: Math.max(a.top, b.top),
+            bottom: Math.min(a.bottom, b.bottom),
+            left: a.left,
+            right: a.right,
+            height: 0,
+            width: 0
+        };
+
+        result.height = result.bottom - result.top;
+        result.width = result.right - result.left;
+
+        return result;
+
+    }
 
 }
