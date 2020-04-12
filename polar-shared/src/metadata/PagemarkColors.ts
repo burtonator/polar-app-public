5d693c92e43544cc3a0c197c06418ac1ef2ba22a
burtonator <burtonator@gmail.com>
2020-04-11 20:09:39 -0600

    pagemarks display now... so only 2 more tasks left before the hump is cleared...

---
+++ polar-shared/src/metadata/PagemarkColors.ts
@@ -0,0 +1,64 @@
+import {PagemarkMode} from "./PagemarkMode";
+import {IPagemark} from "./IPagemark";
+
+export namespace PagemarkColors {
+
+    export interface PagemarkColor {
+        readonly backgroundColor: string;
+        readonly opacity: number;
+    }
+
+    const PagemarkColors = {
+
+        BLUE: {
+            backgroundColor: "#00CCFF",
+            opacity: 0.3
+        },
+
+        LIGHTBLUE: {
+            backgroundColor: "#00CCFF",
+            opacity: 0.15
+        },
+
+        GREY: {
+            backgroundColor: "rgb(125, 125, 125)",
+            opacity: 0.3
+        }
+
+    };
+
+    export function toPagemarkColor(pagemark: IPagemark): PagemarkColor {
+
+        if (! pagemark) {
+            return PagemarkColors.BLUE;
+        }
+
+        if (!pagemark.mode) {
+            return PagemarkColors.BLUE;
+        }
+
+        switch (pagemark.mode) {
+
+            case PagemarkMode.IGNORED:
+                return PagemarkColors.GREY;
+
+            case PagemarkMode.TABLE_OF_CONTENTS:
+                return PagemarkColors.GREY;
+
+            case PagemarkMode.APPENDIX:
+                return PagemarkColors.GREY;
+
+            case PagemarkMode.REFERENCES:
+                return PagemarkColors.GREY;
+
+            case PagemarkMode.PRE_READ:
+                return PagemarkColors.LIGHTBLUE;
+
+            default:
+                return PagemarkColors.BLUE;
+
+        }
+
+    }
+
+}
