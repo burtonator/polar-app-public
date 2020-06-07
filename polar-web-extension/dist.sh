817facc718b03c3f06a408cd44bc10b296781ebf
burtonator <burtonator@gmail.com>
2020-06-07 16:22:20 -0600

    more cleanup...

--- polar-web-extension/dist.sh
+++ polar-web-extension/dist.sh
@@ -8,9 +8,4 @@ cp *.js ${DEST}
 cp *.html ${DEST}
 cp manifest.json ${DEST}
 
-cp --parents ./node_modules/bootstrap/dist/css/bootstrap.min.css ${DEST}
-cp --parents ./node_modules/bootstrap/dist/css/bootstrap-grid.min.css ${DEST}
-cp --parents ./node_modules/bootstrap/dist/css/bootstrap-reboot.min.css ${DEST}
-cp --parents ./node_modules/@fortawesome/fontawesome-free/css/all.min.css ${DEST}
-
 zip -r save-to-polar.zip save-to-polar
