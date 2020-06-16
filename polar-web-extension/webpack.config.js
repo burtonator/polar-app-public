22faf507e726d23a609931a7a6810f876c8f462c
burtonator <burtonator@gmail.com>
2020-06-15 21:45:19 -0600

    - workaround for loading the pdf worker.

--- polar-web-extension/webpack.config.js
+++ polar-web-extension/webpack.config.js
@@ -70,7 +70,36 @@ module.exports = {
 
                 ]
 
-            }
+            },
+            {
+                test: /\.css$/i,
+                include: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
 
         ]
     },
