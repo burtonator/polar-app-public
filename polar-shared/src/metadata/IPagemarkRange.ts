03227fdff8b0b263b39a71ee0cb7e1d95ad2cb39
burtonator <burtonator@gmail.com>
2020-08-20 09:35:34 -0600

    more work on getting epub pagemarks to work properly.

--- polar-shared/src/metadata/IPagemarkRange.ts
+++ polar-shared/src/metadata/IPagemarkRange.ts
@@ -3,28 +3,7 @@ import {IPagemarkAnchor} from "./IPagemarkAnchor";
 /**
  * A pagemark range that can cover a prefix, suffix, and the entire page.
  */
-export type IPagemarkRange = IPagemarkRangePrefix | IPagemarkRangeSuffix | IPagemarkRangeFull;
-
-/**
- * A pagemark range that is a prefix coverage that covers from the start of
- * the document up until the 'end'.
- */
-export interface IPagemarkRangePrefix {
-    readonly end: IPagemarkAnchor;
-}
-
-/**
- * A pagemark range that is a suffix coverage that covers from the end of
- * the document up until the 'start'.
- */
-export interface IPagemarkRangeSuffix {
-    readonly start: IPagemarkAnchor;
-}
-
-/**
- * A full range that covers an explicit start and end range denoted by CFIs.
- */
-export interface IPagemarkRangeFull {
-    readonly start: IPagemarkAnchor;
-    readonly end: IPagemarkAnchor;
+export interface IPagemarkRange {
+    readonly start?: IPagemarkAnchor;
+    readonly end?: IPagemarkAnchor;
 }
