20156fc827dac2dfee96b952adfc5f440b4116f4
burtonator <burtonator@gmail.com>
2020-09-04 14:08:16 -0600

    ...

--- polar-web-extension/src/background.ts
+++ polar-web-extension/src/background.ts
@@ -3,12 +3,13 @@ import {ImportContentHandler} from './legacy/ImportContentHandler';
 import {BrowserScreenshotHandler} from './BrowserScreenshotHandler';
 import {ExtensionInstallHandler} from './ExtensionInstallHandler';
 import {SaveToPolarHandler} from "./services/SaveToPolarHandler";
+import {PingHandler} from "./services/PingHandler";
 
 InitialSplash.register();
 ImportContentHandler.register();
 BrowserScreenshotHandler.register();
 ExtensionInstallHandler.register();
 SaveToPolarHandler.register();
-
+PingHandler.start();
 
 
