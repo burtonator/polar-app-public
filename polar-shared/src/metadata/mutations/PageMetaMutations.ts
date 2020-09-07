e6c83151354ef1578da0e1640fff76eda4b2931b
burtonator <burtonator@gmail.com>
2020-09-06 19:59:42 -0600

    more cleanup of FIXMEs

--- polar-shared/src/metadata/mutations/PageMetaMutations.ts
+++ polar-shared/src/metadata/mutations/PageMetaMutations.ts
@@ -1,14 +1,14 @@
-import {IDocMeta} from "../IDocMeta";
 import {IPageMeta, PageNumber} from "../IPageMeta";
 import {Logger} from "../../logger/Logger";
 import {ISODateTimeStrings} from "../ISODateTimeStrings";
-import {
-    IAnnotationRefWithDocMeta,
-    IIDRef,
-    IPageRef
-} from "../AnnotationRefs";
+import {IAnnotationRefWithDocMeta, IIDRef, IPageRef} from "../AnnotationRefs";
 import {arrayStream} from "../../util/ArrayStreams";
 import {DocMetas} from "../DocMetas";
+import {AnnotationType} from "../AnnotationType";
+import {IAreaHighlight} from "../IAreaHighlight";
+import {IImage} from "../IImage";
+import {ITextHighlight} from "../ITextHighlight";
+import {isPresent} from "../../Preconditions";
 
 const log = Logger.create();
 
@@ -82,6 +82,25 @@ export abstract class MutatorDelegate<V extends IIDRef> {
             console.log("Deleted record: ", ref);
             delete values[ref.id];
             return true;
+        }
+
+        function getImage(): IImage | undefined {
+
+            if (ref.annotationType === AnnotationType.AREA_HIGHLIGHT) {
+                return (<IAreaHighlight> ref.original).image;
+            }
+
+            if (ref.annotationType === AnnotationType.TEXT_HIGHLIGHT) {
+                return (<ITextHighlight> ref.original).image;
+            }
+
+            return undefined;
+        }
+
+        const image = getImage();
+
+        if (image && isPresent(docMeta.docInfo.attachments)) {
+            delete docMeta.docInfo.attachments[image.id];
         }
 
         log.warn("Failed to delete value: ", ref);
