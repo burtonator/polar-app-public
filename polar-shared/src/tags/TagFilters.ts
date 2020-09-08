142f5659c6ef5cc2d713a1a797b0a3cfadc661f5
burtonator <burtonator@gmail.com>
2020-09-08 17:38:33 -0600

    - new TagFilters to using these with streams. - RelatedTagsManager.ts for filtering folder tags

---
+++ polar-shared/src/tags/TagFilters.ts
@@ -0,0 +1,19 @@
+import {Tag} from "./Tags";
+
+export namespace TagFilters {
+
+    /**
+     * Only folders (no tags).
+     */
+    export function onlyFolderTags(tag: Tag) {
+        return tag.label.startsWith('/');
+    }
+
+    /**
+     * Only tags (no folders).
+     */
+    export function onlyRegular<T extends Tag>(tag: T) {
+        return ! tag.label.startsWith('/');
+    }
+
+}
