47cca64241ef37b3345ccc6ada0b62c390dddf46
Kevin Burton <burtonator@gmail.com>
2020-09-23 16:10:33 -0600

    no padding top

--- polar-shared/src/util/Caches.ts
+++ polar-shared/src/util/Caches.ts
@@ -1,3 +1,17 @@
 export namespace Caches {
 
+    export async function test() {
+
+        // we can stick a raw blob as a response with the constructor
+        const cache = await caches.open('test1');
+
+        // headers , status and statusText can be specified
+        // new Response(blob, {})
+        // await cache.put(request, response
+        // )
+
+        // we can put it into the cache, then redirect the user to the home doc page, then pop up a task bar
+
+    }
+
 }
