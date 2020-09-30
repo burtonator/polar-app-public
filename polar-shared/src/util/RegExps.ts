5afdf056ebb832c06bd2821603de1a43fd0445ef
Kevin Burton <burtonator@gmail.com>
2020-09-30 12:46:19 -0600

    ability to parse out the cloze deletions so we pick teh right number...

--- polar-shared/src/util/RegExps.ts
+++ polar-shared/src/util/RegExps.ts
@@ -1,7 +1,29 @@
-export class RegExps {
+export namespace RegExps {
 
-    public static escape(str: string): string {
+    export function escape(str: string): string {
         return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    }
+
+    /**
+     * Convert all matches to an array so we can use streams on the data.
+     */
+    export function matches(re: RegExp, text: string): ReadonlyArray<RegExpExecArray> {
+
+        const result: RegExpExecArray[] = [];
+
+        while(true) {
+
+            const match = re.exec(text);
+            if (match === null) {
+                break;
+            }
+
+            result.push(match);
+
+        }
+
+        return result;
+
     }
 
 }
