67aada94688cbe739e85de3e3bcdbc78566c0ecd
Kevin Burton <burtonator@gmail.com>
2020-10-16 09:07:32 -0600

    ... PDF title extractor work...

--- polar-pdf/src/pdf/PDFTitleExtractorTest.ts
+++ polar-pdf/src/pdf/PDFTitleExtractorTest.ts
@@ -19,9 +19,10 @@ xdescribe('PDFTitleExtractor', function() {
 
     }
 
-    xit("basic", async function () {
-        await doTest('/Users/burton/Downloads/2008.05300.pdf', 'Who Watches the Watchmen?  A Review of Subjective Approaches for Sybil-resistance in Proof of Personhood Protocols')
-        await doTest('/Users/burton/Downloads/p3181-adams.pdf', 'Monarch: Google’s Planet-Scale In-MemoryTime Series Database')
+    it("basic", async function () {
+        // await doTest('/Users/burton/Downloads/2008.05300.pdf', 'Who Watches the Watchmen?  A Review of Subjective Approaches for Sybil-resistance in Proof of Personhood Protocols')
+        // await doTest('/Users/burton/Downloads/p3181-adams.pdf', 'Monarch: Google’s Planet-Scale In-MemoryTime Series Database')
+        await doTest('/Users/burton/projects/polar-app/packages/polar-test-data/pdf/klenke_probability_theory.pdf', 'Monarch: Google’s Planet-Scale In-MemoryTime Series Database')
     });
 
 });
