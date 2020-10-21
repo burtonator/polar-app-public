443699bca80f6be00703fc905ce72486ab1100a8
Kevin Burton <burtonator@gmail.com>
2020-10-21 16:41:10 -0600

    ....

--- polar-page-metadata-parser/src/parsers/ARXIVParser.ts
+++ polar-page-metadata-parser/src/parsers/ARXIVParser.ts
@@ -1,8 +1,9 @@
 import {Parser} from "../Parser";
+import {PageMetadata} from "../PageMetadata";
 
 export class ARXIVParser implements Parser {
 
-    public parse(doc: Document): Metadata | undefined {
+    public parse(doc: Document): PageMetadata | undefined {
         const authors = doc.querySelectorAll(".authors .descriptor");
         // TODO: parse author data...
 
