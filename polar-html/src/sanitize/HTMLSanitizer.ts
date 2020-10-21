50c65f39b253bfda512639fb0f13762c36cdff41
Kevin Burton <burtonator@gmail.com>
2020-10-20 20:18:32 -0600

    handle paste properly ...

--- polar-html/src/sanitize/HTMLSanitizer.ts
+++ polar-html/src/sanitize/HTMLSanitizer.ts
@@ -70,6 +70,60 @@ export namespace HTMLSanitizer {
 
     }
 
+    export function sanitizePasteData(html: string) {
+
+        return sanitizeHtml(html, {
+
+            // TODO: add all of these below.. to allowedAttributes.
+            allowedTags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote',
+                           'cite', 'p', 'a', 'ul', 'ol', 'nl', 'li', 'b', 'i',
+                           'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+                           'table', 'thead', 'caption', 'tbody', 'tr', 'th',
+                           'td', 'pre' ],
+
+            allowedAttributes: {
+
+                'pre': ["style"],
+                'ul': ["style"],
+                'ol': ["style"],
+                'li': ["style"],
+                'ni': ["style"],
+                'code': ["style"],
+                'p': ["style"],
+                'div': ["style"],
+                'span': ["style"],
+                'b': ["style"],
+
+                'blockquote': ["style", "cite"],
+                "a": [ 'style', 'href', 'name', 'target', 'rel', 'type' ],
+                "img": [ 'style', 'src', 'title', 'alt', 'width', 'height' ]
+
+            },
+            allowedStyles: {
+                '*': {
+
+                    // TODO: top,bottom,left,right versions of many of these
+
+                    'font-weight': [/.*/],
+                    'text-decoration': [/.*/],
+                    'text-transform': [/.*/],
+                    'text-indent': [/.*/],
+                    'line-height': [/.*/],
+                    'letter-spacing': [/.*/],
+                    'direction': [/.*/],
+                    'word-spacing': [/.*/],
+                    'white-space': [/.*/],
+                    'transform': [/.*/],
+
+                },
+
+            }
+
+        });
+
+    }
+
+
     /**
      * Sanitize a document so that it's in a 'portable' format of HTML that can
      * reflow and we can use in our EPUB system.
