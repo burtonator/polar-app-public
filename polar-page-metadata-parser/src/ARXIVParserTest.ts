625399d65cc9c4d8644455df52a1d0edcfd8a8c2
Kevin Burton <burtonator@gmail.com>
2020-10-21 10:55:29 -0600

    more infra that works...

---
+++ polar-page-metadata-parser/src/ARXIVParserTest.ts
@@ -0,0 +1,23 @@
+import {assert} from "chai";
+
+describe('ARXIVParser', function() {
+
+    it("basic", function() {
+
+        // TODO: we will have to use JSDOM to create a DOM within nodejs to parse the HTML
+        //
+        // TODO: we should probably save HTML in polar-page-metadata-parser/src/test/arxiv1.html
+        // which will hold test1 and you can use Files to open the test , parse it with JSDOM,
+        // and then you'll have a document object that you can use to test the parser with.
+        // this uses the *same* DOM API that's used in the browser so your code will work there
+        // too
+
+        //
+        // const url = "https://arxiv.org/abs/2010.09039";
+        //
+        // const parser = Parsers.get(url);
+        //
+
+    });
+
+});
--- polar-page-metadata-parser/src/ParsersTest.ts
+++ polar-page-metadata-parser/src/ParsersTest.ts
@@ -1,5 +1,1 @@
 
-
-const url = "https://arxiv.org/abs/2010.09039";
-
-const parser = Parsers.get(url);
