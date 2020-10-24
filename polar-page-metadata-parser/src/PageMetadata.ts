9f64603937a1114ca8657ae02904db4ee9a64121
Kevin Burton <burtonator@gmail.com>
2020-10-24 15:06:03 -0600

    ...

--- polar-page-metadata-parser/src/PageMetadata.ts
+++ polar-page-metadata-parser/src/PageMetadata.ts
@@ -6,6 +6,10 @@
 // and we can probably just extend this class with PageMetadata which will allow
 // us to put EXTRA fields in PageMetadata if necessary
 
+// TODO: we need to support the concept of docMeta that don't have metadata backing them.
+
+export type CaptureType = '';
+
 export interface PageMetadata {
 
     readonly url: string;
