dd297e1b185c6c1ecb3c70bad5b412bf94e7ac44
rongpan <22309073+rongpan@users.noreply.github.com>
2020-10-25 21:59:50 -0500

    arxiv parser

--- polar-page-metadata-parser/src/parsers/ARXIVParser.ts
+++ polar-page-metadata-parser/src/parsers/ARXIVParser.ts
@@ -4,8 +4,64 @@ import {PageMetadata} from "../PageMetadata";
 export class ARXIVParser implements Parser {
 
     public parse(doc: Document): PageMetadata | undefined {
-        const authors = doc.querySelectorAll(".authors .descriptor");
+        // const authors = doc.querySelectorAll(".authors .descriptor");
         // TODO: parse author data...
+
+        var url : string | null = null;
+        var pdfURL : string | null = null;
+        var title : string | null = null;
+        var authors : string[] = [];
+        var summary : string | null = null;
+
+        for (const metaInfo of Array.from(doc.getElementsByTagName("meta"))) {
+            if (metaInfo.attributes.length != 2) {
+                continue;
+            }
+
+            if (metaInfo.attributes[0].nodeName == 'property' && metaInfo.attributes[0].nodeValue == 'og:url') {
+                if (metaInfo.attributes[1].nodeName == 'content') {
+                    url = metaInfo.attributes[1].nodeValue
+                }
+            }
+
+            if (metaInfo.attributes[0].nodeName == 'name' && metaInfo.attributes[0].nodeValue == 'citation_pdf_url') {
+                if (metaInfo.attributes[1].nodeName == 'content') {
+                    pdfURL = metaInfo.attributes[1].nodeValue
+                }
+            }
+
+            if (metaInfo.attributes[0].nodeName == 'name' && metaInfo.attributes[0].nodeValue == 'citation_title') {
+                if (metaInfo.attributes[1].nodeName == 'content') {
+                    title = metaInfo.attributes[1].nodeValue
+                }
+            }
+
+            if (metaInfo.attributes[0].nodeName == 'name' && metaInfo.attributes[0].nodeValue == 'citation_author') {
+                if (metaInfo.attributes[1].nodeName == 'content') {
+                    if (metaInfo.attributes[1].nodeValue != null) {
+                        authors.push(metaInfo.attributes[1].nodeValue)
+                    }
+                }
+            }
+
+            if (metaInfo.attributes[0].nodeName == 'property' && metaInfo.attributes[0].nodeValue == 'og:description') {
+                if (metaInfo.attributes[1].nodeName == 'content') {
+                    summary = metaInfo.attributes[1].nodeValue
+                }
+            }
+
+        }
+
+        if (url != null && pdfURL != null) {
+            const result : PageMetadata = {
+                url: url,
+                pdfURL: pdfURL,
+                title: title == null ? undefined : title,
+                authors: authors.length == 0 ? undefined : authors,
+                summary: summary == null ? undefined : summary
+            };
+            return result;
+        }
 
         return undefined;
 
--- polar-page-metadata-parser/src/parsers/ARXIVParserTest.ts
+++ polar-page-metadata-parser/src/parsers/ARXIVParserTest.ts
@@ -1,8 +1,12 @@
 import {assert} from "chai";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { Fetches } from "polar-shared/src/util/Fetch";
+import { DOMParser } from 'xmldom';
+import { ARXIVParser } from './ARXIVParser';
 
-describe('ARXIVParser', function() {
+describe('ARXIVParser', function () {
 
-    it("basic", function() {
+    it("basic", async function () {
 
         // TODO: we will have to use JSDOM to create a DOM within nodejs to parse the HTML
         //
@@ -17,6 +21,53 @@ describe('ARXIVParser', function() {
         //
         // const parser = Parsers.get(url);
         //
+
+        var text : string = '';
+        const url = "https://arxiv.org/abs/2010.09039";
+
+        // if test file exists
+        if (existsSync('src/test/arxiv1.html')) {
+            console.log('The path exists.');
+            text = readFileSync('src/test/arxiv1.html').toString()
+        
+        // else fetch test html
+        } else {
+            const response = await Fetches.fetch(url);
+    
+            text = await response.text();
+            writeFileSync('src/test/arxiv1.html', text)
+        }
+
+        const parser = new DOMParser();
+        const doc : Document = parser.parseFromString(text, "text/xml");
+
+        const arxivParser = new ARXIVParser();
+        const data = arxivParser.parse(doc)
+
+        if (data === undefined) {
+            assert(false);
+        } else {
+            assert(data.url === 'https://arxiv.org/abs/2010.09039v1')
+            assert(data.pdfURL === 'https://arxiv.org/pdf/2010.09039')
+            assert(data.title === 'On licenses for [Open] Hardware')
+
+            if (data.authors === undefined) {
+                assert(false);
+            } else {
+                assert(data.authors[0] === 'Montón, Màrius')
+                assert(data.authors[1] === 'Salazar, Xavier')
+            }
+            
+            assert(data.summary === `This document explains the basic concepts related to software and hardware
+licenses, and it summarizes the most popular licenses that are currently used
+for hardware projects. Two case studies of hardware projects at different
+levels of abstraction are also presented, together with a discussion of license
+applicability, commercial issues, code protection, and related concerns. This
+paper intends to help the reader understand how to release open hardware with
+the most appropriate license, and to answer questions that are of current
+interest. We have been mainly motivated by the growing influence of the open
+RISC-V ISA, but trying to address a wider hardware point of view.`)
+        }
 
     });
 
