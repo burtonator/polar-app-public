4cacb8ac4595dceb1fd6ddeee63bfda05de44af3
rongpan <22309073+rongpan@users.noreply.github.com>
2020-10-30 00:54:43 -0500

    arxiv parser with jsdom

--- polar-page-metadata-parser/src/parsers/ARXIVParser.ts
+++ polar-page-metadata-parser/src/parsers/ARXIVParser.ts
@@ -4,66 +4,29 @@ import {PageMetadata} from "../PageMetadata";
 export class ARXIVParser implements Parser {
 
     public parse(doc: Document): PageMetadata | undefined {
-        // const authors = doc.querySelectorAll(".authors .descriptor");
-        // TODO: parse author data...
 
-        var url : string | null = null;
-        var pdfURL : string | null = null;
-        var title : string | null = null;
-        var authors : string[] = [];
-        var summary : string | null = null;
+        const title = doc.querySelector("meta[name='citation_title']")?.getAttribute("content") || undefined
 
-        for (const metaInfo of Array.from(doc.getElementsByTagName("meta"))) {
-            if (metaInfo.attributes.length != 2) {
-                continue;
+        const description = doc.querySelector("meta[property='og:description']")?.getAttribute("content") || undefined
+
+        var authors : string[] = []
+
+        for (const metaInfo of Array.from(doc.querySelectorAll("meta[name='citation_author']"))) {
+            const author = metaInfo.getAttribute("content")
+            if (author !== null) {
+                authors.push(author)
             }
-
-            if (metaInfo.attributes[0].nodeName == 'property' && metaInfo.attributes[0].nodeValue == 'og:url') {
-                if (metaInfo.attributes[1].nodeName == 'content') {
-                    url = metaInfo.attributes[1].nodeValue
-                }
-            }
-
-            if (metaInfo.attributes[0].nodeName == 'name' && metaInfo.attributes[0].nodeValue == 'citation_pdf_url') {
-                if (metaInfo.attributes[1].nodeName == 'content') {
-                    pdfURL = metaInfo.attributes[1].nodeValue
-                }
-            }
-
-            if (metaInfo.attributes[0].nodeName == 'name' && metaInfo.attributes[0].nodeValue == 'citation_title') {
-                if (metaInfo.attributes[1].nodeName == 'content') {
-                    title = metaInfo.attributes[1].nodeValue
-                }
-            }
-
-            if (metaInfo.attributes[0].nodeName == 'name' && metaInfo.attributes[0].nodeValue == 'citation_author') {
-                if (metaInfo.attributes[1].nodeName == 'content') {
-                    if (metaInfo.attributes[1].nodeValue != null) {
-                        authors.push(metaInfo.attributes[1].nodeValue)
-                    }
-                }
-            }
-
-            if (metaInfo.attributes[0].nodeName == 'property' && metaInfo.attributes[0].nodeValue == 'og:description') {
-                if (metaInfo.attributes[1].nodeName == 'content') {
-                    summary = metaInfo.attributes[1].nodeValue
-                }
-            }
-
         }
+        
+        const pdfURL = doc.querySelector("meta[name='citation_pdf_url']")?.getAttribute("content") || undefined
 
-        if (url != null && pdfURL != null) {
-            const result : PageMetadata = {
-                url: url,
-                pdfURL: pdfURL,
-                title: title == null ? undefined : title,
-                authors: authors.length == 0 ? undefined : authors,
-                summary: summary == null ? undefined : summary
-            };
-            return result;
-        }
-
-        return undefined;
+        return {
+            url: doc.location.href,
+            title,
+            description,
+            authors,
+            pdfURL
+        };
 
     }
 
