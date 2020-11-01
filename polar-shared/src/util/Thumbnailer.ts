0a0a68e5e07a005e5c6907d91df78d5de3b99652
Kevin Burton <burtonator@gmail.com>
2020-10-31 21:50:47 -0600

    ckeditor5...

--- polar-shared/src/util/Thumbnailer.ts
+++ polar-shared/src/util/Thumbnailer.ts
@@ -1,10 +1,34 @@
 import {PathOrURLStr} from "./Strings";
 import {IDimensions} from "./IDimensions";
 
+export type DataURL = string;
+
+export type ImageType = 'image/png' | 'image/jpeg' | 'image/webp' | 'image/gif';
+
 export interface ThumbnailerGenerateOpts {
     readonly pathOrURL: PathOrURLStr;
     readonly scaleBy: 'width' | 'height';
     readonly value: number;
+}
+
+export interface ImageData {
+
+    /**
+     * The actual data of the image.
+     */
+    readonly data: ArrayBuffer | DataURL;
+
+    /**
+     * The internal format of the 'data'
+     */
+    readonly format: 'arraybuffer' | 'dataurl';
+
+    readonly type: ImageType;
+
+    readonly width: number;
+
+    readonly height: number;
+
 }
 
 export interface IThumbnail extends ImageData {
