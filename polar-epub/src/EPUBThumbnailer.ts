1cb835d7f6ad0599ba82c8dfc6e97e0d56e573b1
Kevin Burton <burtonator@gmail.com>
2020-10-31 23:12:37 -0600

    I think I have EPUB sort of working for image resize.

--- polar-epub/src/EPUBThumbnailer.ts
+++ polar-epub/src/EPUBThumbnailer.ts
@@ -10,6 +10,23 @@ export namespace EPUBThumbnailer {
         const metadata = await book.loaded.metadata;
         const spine = await book.loaded.spine;
 
+        const coverURL = await book.coverUrl()
+
+        if (! coverURL) {
+            return undefined;
+        }
+
+        console.log("FIXME: coverURL: ", coverURL);
+
+        const response = await fetch(coverURL);
+
+        const blob = await response.blob();
+
+        console.log("FIXME: headers: ", response.headers);
+        console.log("FIXME: blob type: ", blob.type);
+
+        return undefined;
+
     }
 
 
