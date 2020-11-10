8136e54f6d581f6a6650ae8406c03dc878aa3086
Kevin Burton <burtonator@gmail.com>
2020-11-10 10:14:14 -0700

    - more potential fixed by logging issues fixed with Mailchimp.ts - new polar-readability package for testing Readability - new FetchesTestingCache.ts for caching network resources - tracing with useLogWhenChanged traces both states

---
+++ polar-readability/src/ReadabilityTest.ts
@@ -0,0 +1,34 @@
+import Readability from "readability";
+import {ConstructorOptions, JSDOM} from "jsdom";
+import {FetchesTestingCache} from "polar-shared/src/util/FetchesTestingCache";
+
+describe('Readability', function () {
+    function parseHTML(html: string, url: string) {
+        const opts: ConstructorOptions = {url, contentType: 'text/html', resources: 'usable'};
+        const dom = new JSDOM(html, opts);
+        return dom.window.document;
+    }
+
+    it("basic", async function () {
+
+        const url = 'https://www.bunniestudios.com/blog/?p=5971';
+        const html = await FetchesTestingCache.fetch(url);
+
+        const doc = parseHTML(html, url)
+
+        console.log(process.cwd());
+
+        const readability = new Readability(doc);
+        const readable = readability.parse();
+
+        // TODO this isn't working because I'm getting the result of:
+        // <div id="readability-page-1" class="page">[object Promise]</div>
+        //
+        // so maybe promise aren't working in readability.
+
+        console.log("title: " + readable.title);
+        console.log("content: \n" + readable.content);
+
+    });
+
+});
---
+++ polar-shared/src/util/FetchesTestingCache.ts
@@ -0,0 +1,39 @@
+import { Files } from "./Files";
+import { Hashcodes } from "./Hashcodes";
+import {Fetches} from "./Fetch";
+
+export namespace FetchesTestingCache {
+
+    async function computePath(url: string) {
+
+        await Files.createDirAsync('test')
+        await Files.createDirAsync('test/fetch-cache')
+
+        const hashcode = Hashcodes.create(url);
+
+        return `test/fetch-cache/${hashcode}.dat`;
+
+    }
+
+    export async function fetch(url: string): Promise<string> {
+
+        const path = await computePath(url);
+
+        const exists = await Files.existsAsync(path);
+
+        if (exists) {
+            return Files.readFileAsync(path).toString()
+        } else {
+
+            const response = await Fetches.fetch(url);
+
+            const html = await response.text();
+            console.log(`Writing cache path ${path} for URL: ${url}`);
+            await Files.writeFileAsync(path, html)
+            return html;
+
+        }
+
+    }
+
+}
