0bbc831a5b83de7f6065554c0ab349f8c9785e88
Kevin Burton <burtonator@gmail.com>
2020-11-27 17:36:15 -0700

    markdown to html and vice versa...

---
+++ polar-markdown-parser/src/HTMLToMarkdownTest.ts
@@ -0,0 +1,14 @@
+import {assert} from 'chai';
+import {HTMLToMarkdown} from "./HTMLToMarkdown";
+import html2markdown = HTMLToMarkdown.html2markdown;
+
+describe('HTMLToMarkdown', function() {
+
+    it('basic', function() {
+
+        assert.equal(html2markdown('<p><strong>this is bold</strong></p>'), '**this is bold**');
+
+    });
+
+
+});
---
+++ polar-markdown-parser/src/MarkdownToHTMLTest.ts
@@ -0,0 +1,14 @@
+import {MarkdownToHTML} from "./MarkdownToHTML";
+import markdown2html = MarkdownToHTML.markdown2html;
+import {assert} from 'chai';
+
+describe('MarkdownToHTML', function() {
+
+    it('basic', function() {
+
+        assert.equal(markdown2html('**this is bold**'), '<p><strong>this is bold</strong></p>\n');
+
+    });
+
+
+});
