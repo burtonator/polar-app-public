2abcc8d8b6f0290b36cb579be6ae626e2f3d5dca
Kevin Burton <burtonator@gmail.com>
2021-02-28 10:10:54 -0700

    initial support for some paste handlers...

--- polar-shared/src/util/ImageType.ts
+++ polar-shared/src/util/ImageType.ts
@@ -1,1 +1,1 @@
-export type ImageType = 'image/png' | 'image/jpeg' | 'image/webp' | 'image/gif';
+export type ImageType = 'image/png' | 'image/jpeg' | 'image/webp' | 'image/gif' | 'image/svg+xml' | 'image/avif' | 'image/apng';
