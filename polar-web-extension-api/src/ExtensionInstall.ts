c6fb694b7151681d8f821c29b3c99eafc7be567e
Kevin Burton <burtonator@gmail.com>
2021-03-20 21:44:58 -0600

    Fixed compilation issue after typescript upgrade...

--- polar-web-extension-api/src/ExtensionInstall.ts
+++ polar-web-extension-api/src/ExtensionInstall.ts
@@ -1,10 +1,11 @@
 import {WebExtensions} from './WebExtensions';
+import {isPresent} from "polar-shared/src/Preconditions";
 
 export class ExtensionInstall {
 
     public static async isInstalled(): Promise<boolean> {
 
-        if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
+        if (chrome && chrome.runtime && isPresent(chrome.runtime.sendMessage)) {
             const result = await WebExtensions.Messaging.sendMessage({});
             return result !== null;
         }
