9ed30e070c9b6f8e678ceca4c3cee4954e2e2a1d
Kevin Burton <burtonator@gmail.com>
2021-05-05 20:18:43 -0500

    all these were inter-related...

--- polar-shared/src/metadata/blocks/ICodeNote.ts
+++
@@ -1,9 +0,0 @@
-import {IBlock} from "./IBlock";
-
-export type CodeStr = string;
-
-export interface ICodeNote extends IBlock {
-    readonly type: 'code';
-    readonly lang: 'typescript' | 'javascript' | 'markdown' | 'java' | string;
-    readonly value: CodeStr;
-}
--- polar-shared/src/metadata/blocks/ILatexNote.ts
+++
@@ -1,8 +0,0 @@
-import {IBlock} from "./IBlock";
-
-export type LatexStr = string;
-
-export interface ILatexNote extends IBlock {
-    readonly type: 'latex';
-    readonly data: LatexStr;
-}
